177f6b9fac4edf2ea69cca07bf87ec83775c1be7
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-19 22:13:59 +0300

    Client <react> created myrecipes page

--- src/pages/MyRecipes/index.jsx
+++ src/pages/MyRecipes/index.jsx
@@ -1,11 +1,30 @@
 import '../MyRecipes/style.css';
 import Nav from '../../components/Nav/index';
+import Card from '../../components/Card/index';
+import Footer from '../../components/Footer/index';
 
 const MyRecipes = () => {
+
+    let id= 1;
+    let name = 'Tabouleh';
+
     return(
         <div className='MyRecipes'>
             <div className='MyRecipes_nav'><Nav/></div>
-            MyRecipes
+            <div className='MyRecipes_header'>
+                <div className='page_title'><h1>My Recipes</h1></div>
+                <div className="addRecipe"><button>Add New Recipe</button></div>
+            </div>
+            <div className="cardContainer">
+                <Card id={id} name={name}/>
+                <Card id={id} name={name}/>
+                <Card id={id} name={name}/>
+                <Card id={id} name={name}/>
+                <Card id={id} name={name}/>
+                <Card id={id} name={name}/>
+                <Card id={id} name={name}/>
+            </div>
+            <div><Footer/></div>
         </div>
     );
 }
