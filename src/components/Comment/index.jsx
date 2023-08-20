e7dbee4c5918e84a9ac4e70d42ead6531e01a03c
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-20 16:33:00 +0300

    Client <react> fetched get comments api

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -3,7 +3,7 @@ import '../Comment/style.css';
 const CommentsContainer = (props) => {
     return(
         <div className='Comment'>
-            <div className='username'>{props.name}</div>
+            <div className='username'>{props.first_name} {props.last_name}</div>
             <div className='text'>{props.text}</div>
         </div>
     );
--- src/components/CommentsContainer/index.jsx
+++ src/components/CommentsContainer/index.jsx
@@ -1,11 +1,13 @@
 import '../CommentsContainer/style.css';
 import Comment from '../../components/Comment/index';
 import SendRoundedIcon from '@mui/icons-material/SendRounded';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const CommentsContainer = () => {
 
     const [newComment, setNewComment] = useState('');
+    let [comments , setComments] = useState([]);
 
     let id = 2;
     let name = 'omar_krayyem'
@@ -16,14 +18,37 @@ const CommentsContainer = () => {
         setNewComment('');
     };
 
+    let recipe_id = 4;
+
+    const getComments = async () => {
+
+        const token = localStorage.getItem("token");
+
+        await axios.get(`http://127.0.0.1:8000/api/comments/${recipe_id}`, {
+            "headers": {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            console.log(response.data.data);
+            setComments(response.data.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+    
+    useEffect(() => {
+        getComments();
+    }, []);
+
     return(
         <div className='CommentsContainer'>
             <div className="CommentsTitle"><h1>Comments</h1></div>
             <div className='Comments'>
-                <Comment id={id} name={name} text={text}/>
-                <Comment id={id} name={name} text={text}/>
-                <Comment id={id} name={name} text={text}/>
-                <Comment id={id} name={name} text={text}/>
+                {comments.map((comment) => (
+                    <Comment id={comment.id} name={comment.text} text={comment.text} first_name={comment.user.first_name} last_name={comment.user.last_name}/>
+                ))}
             </div>
             <div className="addComment">
                 <input 
