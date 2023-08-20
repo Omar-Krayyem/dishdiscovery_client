c6a83bc0bc105c92fc96ebb8647fbabf968dc95b
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-20 17:19:57 +0300

    Client <react> fetched add calendar api

--- src/components/AddCalendar/index.jsx
+++ src/components/AddCalendar/index.jsx
@@ -1,26 +1,48 @@
-import React from "react";
 import '../AddCalendar/style.css'
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const AddCalendar = ({ onClose, name }) =>{
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
+    const [date, setDate] = useState('');
+
+    let recipe_id = 1;
+    const token = localStorage.getItem("token");
+
+    console.log(date);
+    const addCalendar = (event) => {
+        event.preventDefault();
+
+        const postData = { recipe_id, date };
+        axios.post('http://127.0.0.1:8000/api/calendar/store', postData, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            
+            console.log(response.data);
+            onClose();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    };
 
     return (
         <div className="AddCalendar">
             <div className="AddCalendar_container">
                 <h1 className="title">Add Plan Meal</h1>
-                <form className="CalendarForm" onSubmit={handleSubmit}>
+                <form className="CalendarForm">
                     <div className="text_feild">
                         <label className="CalendarLabel">Recipe Name</label><br></br>
                         <input type="text" value={name} ></input> 
                     </div>
                     <div className="Date">
                         <label className="CalendarLabel">Date</label><br></br>
-                        <input type="date"></input> 
+                        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
                     </div>
-                    <button className="CalendarAddbtn">Add</button>
+                    <button className="CalendarAddbtn" onClick={addCalendar}>Add</button>
                     <button className="CalendarCloebtn" onClick={onClose}>Close</button>
                 </form>
             </div>  
