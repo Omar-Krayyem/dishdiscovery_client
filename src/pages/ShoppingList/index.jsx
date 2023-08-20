43e4e32e7aeb1b67d09d17e9cbdcc8e633adfa90
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-20 09:06:28 +0300

    Client <react> created shopping list page

--- src/pages/ShoppingList/index.jsx
+++ src/pages/ShoppingList/index.jsx
@@ -1,11 +1,49 @@
 import '../ShoppingList/style.css';
 import Nav from '../../components/Nav/index';
-
+import Footer from '../../components/Footer/index';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckIcon from '@mui/icons-material/Check';
 const ShoppingList = () => {
     return(
         <div className='ShoppingList'>
-            <div className='ShoppingList_nav'><Nav/></div>
-            ShoppingList
+            <div className="ShoppingList_top">
+                <div className='ShoppingList_nav'><Nav/></div>
+                <div className='ShoppingList_header'>
+                    <div className='page_title'><h1>Shopping List</h1></div>
+                    <div className="addlistitem"><button>Add New Item</button></div>
+                </div>
+                <div className='shoppingList_body'>
+                    <table className='shoppingList_table'>
+                        <thead className='shoppingList_thead'>
+                            <tr className=''>
+                                <th className='shoppingList_th top_left'>Item Name</th>
+                                <th className='shoppingList_th'>Quantity</th>
+                                <th className='shoppingList_th top_right'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className='shoppingList_tr'>
+                                <td className='shoppingList_td'>Cucumber</td>
+                                <td className='shoppingList_td'>1</td>
+                                <td className='shoppingList_td'><CheckIcon/> <DeleteIcon/> </td>
+                            </tr>
+                            <tr className='shoppingList_tr'>
+                                <td className='shoppingList_td'>Cucumber</td>
+                                <td className='shoppingList_td'>1</td>
+                                <td className='shoppingList_td'><CheckIcon/> <DeleteIcon/></td>
+                            </tr>
+                            <tr className='shoppingList_tr'>
+                                <td className='shoppingList_td'>Cucumber</td>
+                                <td className='shoppingList_td'>1</td>
+                                <td className='shoppingList_td'><CheckIcon/> <DeleteIcon/></td>
+                            </tr>
+                            
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className='ShoppingList_bottom'><div className='footer'><Footer/></div></div>
         </div>
     );
 }
