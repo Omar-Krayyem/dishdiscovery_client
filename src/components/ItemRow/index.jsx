cfcdbe690b7440b1b6840ced3eee190efa1dda1a
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-20 10:41:04 +0300

    Client <react> fetched delete shopping list api

--- src/components/ItemRow/index.jsx
+++ src/components/ItemRow/index.jsx
@@ -1,14 +1,33 @@
 import '../ItemRow/style.css';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
+import axios from 'axios';
 
 const ItemRow = (props) => {
+
+    const deleteItem = (event) => {
+        event.preventDefault();
+        const token = localStorage.getItem("token")
+
+        axios.delete(`http://127.0.0.1:8000/api/list/destroy/${props.id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            console.log(response.data.data);
+            window.location.reload();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
 
     return(
         <tr className='ItemRow_tr' id={props.id}>
             <td className='ItemRow_td'>{props.name}</td>
             <td className='ItemRow_td'>{props.quantity}</td>
-            <td className='ItemRow_td'><CheckIcon/> <DeleteIcon/> </td>
+            <td className='ItemRow_td' onClick={deleteItem}><CheckIcon/> <DeleteIcon/> </td>
         </tr>
     );
 }
--- src/pages/ShoppingList/index.jsx
+++ src/pages/ShoppingList/index.jsx
@@ -15,7 +15,7 @@ const ShoppingList = () => {
         setPopupVisibility(true);
     }
 
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token");
 
     const getItems = async () => {
         await axios.get(`http://127.0.0.1:8000/api/list`, {
