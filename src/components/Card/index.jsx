ffd099f2678c020371587013f6f586c0a98ca649
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-20 23:53:00 +0300

    Client <react> fetched get recipes api

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   return (
     <Link className='card_link' to={``}>
         <div className="card" id="card">
-            <div className="card_img"><img src="https://feelgoodfoodie.net/wp-content/uploads/2023/04/Lebanese-Tabbouleh-Salad-09.jpg" alt=""></img></div>
+            <div className="card_img"><img src={props.new_image_url} alt=""></img></div>
             <div className="card_name">{props.name}</div>
         </div>
     </Link>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,24 +3,41 @@ import Nav from '../../components/Nav/index';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
 import Card from '../../components/Card/index';
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 const Home = () => {
 
-  let id= 1;
-  let name = 'Tabouleh';
+  let [cards, setCards] = useState([]);
+
+  const getRecipes = async () => {
+    const token = localStorage.getItem("token");
+
+    await axios.get(`http://127.0.0.1:8000/api/Home`, {
+        "headers": {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        console.log(response.data.data);
+        setCards(response.data.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
   return (
     <div className='home'>
       <div className='home_nav'><Nav/></div>
       <div className='header'><Header/></div>
       <div className="cardContainer">
-        <Card id={id} name={name}/>
-        <Card id={id} name={name}/>
-        <Card id={id} name={name}/>
-        <Card id={id} name={name}/>
-        <Card id={id} name={name}/>
-        <Card id={id} name={name}/>
-        <Card id={id} name={name}/>
+        {cards.map((card) => (
+         <Card id={card.id} name={card.name} new_image_url={card.new_image_url}/>
+        ))}
       </div>
       <div className='footer'><Footer/></div>
     </div>
