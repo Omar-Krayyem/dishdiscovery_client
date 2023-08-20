ed8ceb2529c1e7e357b6af76d58804feb053ffbb
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-20 22:17:08 +0300

    Client <react> fetched created recipe api

--- src/components/AddRecipe/index.jsx
+++ src/components/AddRecipe/index.jsx
@@ -1,34 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import '../AddRecipe/style.css'
+import axios from 'axios';
 
 const AddRecipe = ({ onClose }) =>{
 
-    const handleSubmit = (e) => {
+    let [name, setName] = useState("");
+    let [cuisine, setCuisine] = useState("");
+    let [ingredients, setIngredients] = useState("");
+    let [image, setImage] = useState("");
+
+    const addRecipe = (e) => {
         e.preventDefault();
+
+        const token = localStorage.getItem("token");
+        const postData = { name, cuisine, ingredients, image };
+
+        console.log(postData)
+    
+        axios.post('http://127.0.0.1:8000/api/recipe/store', postData, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        .then(response => {
+            console.log(response.data);
+            onClose();
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
+
 
     return (
         <div class="AddRecipe">
             <div className="AddRecipe_container">
                 <h1 class="title" >Add Recipe</h1>
-                <form className="RecipreForm" onSubmit={handleSubmit}>
+                <form enctype = 'multipart/form-data' className="RecipreForm">
                     <div class="text_feild">
                         <label className="RecipeLabel">Recipe Name</label><br></br>
-                        <input type="text" placeholder="Recipe Name"></input> 
+                        <input 
+                        type="text" 
+                        placeholder="Recipe Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        ></input> 
                     </div>
                     <div class="text_feild">
                         <label className="RecipeLabel">Cuisine</label><br></br>
-                        <input type="text" placeholder="Cuisine"></input> 
+                        <input 
+                        type="text" 
+                        placeholder="Cuisine"
+                        value={cuisine}
+                        onChange={(e) => setCuisine(e.target.value)}
+                        ></input> 
                     </div>
                     <div class="text_feild">
                         <label className="RecipeLabel">Ingredients</label><br></br>
-                        <textarea type="text" placeholder="Ingredients"></textarea> 
+                        <textarea 
+                        type="text" 
+                        placeholder="Ingredients"
+                        value={ingredients}
+                        onChange={(e) => setIngredients(e.target.value)}
+                        ></textarea> 
                     </div>
                     <div class="upload">
                         <label className="RecipeLabel">Image</label>
-                        <label class="file-label">Choose an image<input type="file" /></label>
+                        <label class="file-label">Choose an image<input 
+                        type="file" 
+                        onChange ={(e) => setImage(e.target.files[0])}
+                        /></label>
                     </div>
-                    <button className="RecipeAddbtn">Add</button>
+                    <button className="RecipeAddbtn" onClick={addRecipe}>Add</button>
                     <button className="RecipeCloebtn" onClick={onClose}>Close</button>
                 </form>
             </div>  
